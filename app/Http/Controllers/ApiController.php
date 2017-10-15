8ebbab11fbc740f2582002cea694dfcd21201517
Plumptre <dplumptre@yahoo.com>
2017-10-15 08:50:21 +0100

    api things

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Picture;
+use App\Category;
 
 class ApiController extends Controller
 {
@@ -14,6 +15,16 @@ class ApiController extends Controller
     public function portfolio()
     {
     $data = Picture::all();
+    return response()->json(['result' => $data]);
+
+    }
+
+
+
+
+    public function category()
+    {
+    $data = Category::all();
     return response()->json(['result' => $data]);
 
     }
